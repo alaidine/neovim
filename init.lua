88bcbe180a964834725aaea610e3cd8689ea1f32
alaidine <aa.alimlinde@gmail.com>
2023-01-08 13:12:54 +0400

    changed the way nvim loads themes

--- init.lua
+++ init.lua
@@ -1,8 +1,13 @@
 require("alaidine.plugins")
 require("alaidine.base")
 require("alaidine.keymaps")
-require("alaidine.colors.onedark")
 require("alaidine.lsp.mason")
 require("alaidine.lsp.lspconfig")
 require("alaidine.lsp.lspsaga")
 require("alaidine.lsp.null-ls")
+
+function set_colors(color)
+	return require("alaidine.colors." .. color)
+end
+
+set_colors("onedark")
